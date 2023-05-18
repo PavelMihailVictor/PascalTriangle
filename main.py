755ae753fd0fc73f84a9edd2aba82c8d0851ef83
Pavel Mihail victor <pavleboss443@yahoo.com>
2023-05-18 14:03:43 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+
+
+
+from math import factorial
+
+def pascal_triangle(n):
+    for i in range(n):
+        for j in range(n-i+1):
+            print(end=' ')
+
+        for j in range(i+1):
+
+            print(factorial(i)//(factorial(j)*factorial(i-j)), end=' ')
+
+        print()
+
+if __name__ == '__main__':
+   pascal_triangle(5)
+
